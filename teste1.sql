6361a44fdaafbd39ad44261c2a7b7277b3ae4718
lubaptista <111128530+lubaptista@users.noreply.github.com>
2022-09-06 15:08:27 -0300

    Add files via upload

---
+++ teste1.sql
@@ -0,0 +1,14 @@
+ use cadastro;alunos
+create table alunos (
+Nome varchar(30),
+RA tinyint,
+Email varchar(50),
+Idade tinyint
+);
+
+describe alunos;
+
+create table materias (
+Nome varchar(30),
+Sala tinyintmateriasalunosmaterias
+);
